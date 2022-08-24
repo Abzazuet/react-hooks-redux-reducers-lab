8d1fd1ac014e94c7889a4d02dff8138513c44495
Abel Zazueta <abzazuet@amazon.com>
2022-08-24 09:32:48 -0700

    Assignment completed

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,19 @@
-export function manageFriends(state, action) {
+const initialState = { friends: [] };
+export function manageFriends(state = initialState, action) {
   // your code here
+  switch (action.type){
+    
+    case "friends/add":
+      let newFriend = {
+        name: action.payload.name,
+        hometown: action.payload.hometown,
+        id: action.payload.id
+      }
+      state.friends.push(newFriend)
+      return {friends: state.friends};
+    case "friends/remove":
+      return {friends: state.friends.filter(friend => friend.id !== action.payload)};
+    default:
+      return state;
+  }
 }
